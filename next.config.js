ff5ccd474de90a89bc3d04858993ac7dcb1bdd4a
perwriters <perwriters@gmail.com>
2024-09-26 05:34:52 +0300

    config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  images: {
+    domains: ['www.paramind.net'], // Add the domain here
+  },
+})
